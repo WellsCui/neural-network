b78cfc9fddce5e82412f338319254e4bc1f40c78
Wei Cui <wells.cui@gmail.com>
2017-09-25 08:55:05 -0400

    add plots

--- wuziqi/plots.py
+++ wuziqi/plots.py
@@ -14,6 +14,23 @@ def plot_policy_net_train_result():
     plt.show()
 
 
+def plot_policy_net_train_result2():
+    with open('../history/logs/policy-dropout-on-layers-validate.txt', 'r') as myfile:
+        data = myfile.read().replace('\n', '')
+        v = np.array(eval(data))
+    with open('../history/logs/policy-dropout-on-layers-accuracy.txt', 'r') as myfile:
+        data = myfile.read().replace('\n', '')
+        t = np.array(eval(data))
+
+    x = np.array(range(v.shape[0]))
+    t = t[x*6]
+    plt.plot(x, t[:, 0], x, t[:, 1], x, t[:, 2], x, v[:, 0], x, v[:, 1], x, v[:, 2])
+    plt.title('Accuracy of Policy Network')
+    plt.xlabel('Iter(5e+1)')
+    plt.ylabel('Accuracy(%)')
+    plt.show()
+
+
 def plot_value_net_train_result():
     with open('../history/logs/value-net-validate.txt', 'r') as myfile:
         data = myfile.read().replace('\n', '')
@@ -26,4 +43,4 @@ def plot_value_net_train_result():
     plt.show()
 
 
-plot_value_net_train_result()
+plot_policy_net_train_result2()
